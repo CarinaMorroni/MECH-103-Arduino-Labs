3b287563f886b97828fdbe7a9d97dedc4bed0777
CarinaMorroni <83429172+CarinaMorroni@users.noreply.github.com>
2021-05-01 19:49:59 -0600

    Add files via upload

---
+++ Lab 6/README.md.txt
@@ -0,0 +1,5 @@
+# Morse Code Arduino
+
+*This Script:*
+* Uses 1s and 0s as the dashes and dots of Morse Code to produce letters of the alphabet
+* Uses the arduino to say "HELP" in Morse Code
---
+++ Lab 6/blinky_Arduino.m
@@ -0,0 +1,18 @@
+% This script holds arrays that represent the Morse code of letters
+% and words that will be sent out as a message
+% Course: MECH 103
+% Name:
+% Date:
+clc; close all;
+H = [1 0 0 1 1 0 1];
+E = [0];
+L = [1 0 1 1 0 1 0 1 ];
+P = [1 0 0 0 0 1 1 1 0];
+
+
+H = [1 0 1 0 1 0 1]
+E = [1]
+L = [1 0 1 1 0 1 0 1]
+P = [1 0 1 1 0 1 1 0 1]
+
+message = [H 0 0 E 0 0 L 0 0 P]
